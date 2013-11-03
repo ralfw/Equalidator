165f3e6f3c4ced891bfea70f8594a20b3591809e
Ralf Westphal <info@ralfw.de>
2013-11-03 16:02:43 +0100

    AppVeyor debugging: dump only src dir

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,5 +1,5 @@
 cd %srcFolder%\nuget
 nuget pack ..\source\equalidator\equalidator\equalidator.csproj
 nuget setApiKey %nugetApiKey%
-tree ..\.. /f /a
+tree .. /f /a
 nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
