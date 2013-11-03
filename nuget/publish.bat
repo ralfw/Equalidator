09adb40aa1e4891836a3ae84f1065470e5b6f2f7
Ralf Westphal <info@ralfw.de>
2013-11-03 15:36:00 +0100

    Added Tree cmd to NuGet publishing for AppVeyor debugging purposes

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,3 +1,4 @@
 %srcFolder%\nuget\nuget pack %srcFolder%\source\equalidator\equalidator\equalidator.csproj
 %srcFolder%\nuget\nuget setApiKey %nugetApiKey%
+tree .. /f /a
 %srcFolder%\nuget\nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
