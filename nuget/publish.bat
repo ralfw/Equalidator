62cc0514b420fe2465553161526163ccad480885
Ralf Westphal <info@ralfw.de>
2013-11-03 15:40:33 +0100

    AppVeyor Debuggig: cd into nuget dir for tree cmd to work

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -1,4 +1,5 @@
 %srcFolder%\nuget\nuget pack %srcFolder%\source\equalidator\equalidator\equalidator.csproj
 %srcFolder%\nuget\nuget setApiKey %nugetApiKey%
+cd %srcFolder%\nuget
 tree .. /f /a
-%srcFolder%\nuget\nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
+nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
