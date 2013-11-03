7f001b9cab671f96cb18ff5a4694382703400e93
Ralf Westphal <info@ralfw.de>
2013-11-03 16:04:54 +0100

    AppVeyor debugging: try to dump build folder

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -2,4 +2,5 @@ cd %srcFolder%\nuget
 nuget pack ..\source\equalidator\equalidator\equalidator.csproj
 nuget setApiKey %nugetApiKey%
 tree .. /f /a
+tree %buildFolder% /f /a
 nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
