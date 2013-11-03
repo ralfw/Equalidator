c4f42ddf4badcd305af42f98a75d7a37f959f950
Ralf Westphal <info@ralfw.de>
2013-11-03 16:07:35 +0100

    AppVeyor debugging: path to buildfolder

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -2,5 +2,5 @@ cd %srcFolder%\nuget
 nuget pack ..\source\equalidator\equalidator\equalidator.csproj
 nuget setApiKey %nugetApiKey%
 tree .. /f /a
-tree %buildFolder% /f /a
+tree %buildFolder%\build /f /a
 nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
