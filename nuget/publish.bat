b9e55a514a07dada4533fefff08dda78c2fdbea0
Ralf Westphal <info@ralfw.de>
2013-11-03 16:11:17 +0100

    AppVeyor debugging: change build folder to "out"

--- nuget/publish.bat
+++ nuget/publish.bat
@@ -2,5 +2,5 @@ cd %srcFolder%\nuget
 nuget pack ..\source\equalidator\equalidator\equalidator.csproj
 nuget setApiKey %nugetApiKey%
 tree .. /f /a
-tree %buildFolder%\build /f /a
+tree %buildFolder%\out /f /a
 nuget push %srcFolder%\nuget\equalidator.%projectVersion%.nupkg
